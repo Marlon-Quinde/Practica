2be8f1661deb49bec6188c8412fb911b8220aa57
Marlon Quinde <71990962+Marlon-Quinde@users.noreply.github.com>
2023-05-11 00:01:52 -0500

    actualizacion 11/05/2023

--- src/app/products/components/price/price.component.ts
+++ src/app/products/components/price/price.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnChanges, OnDestroy, OnInit, SimpleChanges } from '@angular/core';
 
 @Component({
   selector: 'products-price',
   templateUrl: './price.component.html',
   styleUrls: ['./price.component.css']
 })
-export class PriceComponent {
+export class PriceComponent implements OnInit, OnChanges, OnDestroy {
+  @Input() price: number = 0
 
+
+  ngOnDestroy(): void {
+    console.log('Price component ngOnDestroy')
+  }
+  ngOnInit(): void {
+    console.log('Price component ngOnInit')
+  }
+  ngOnChanges(changes: SimpleChanges): void {
+    console.log('Price component ngOnChanges')
+    console.log({changes})
+  }
+
+
+ 
 }
